edc0f4798bc62b4d16d9843f53e8d767ccf5193a
Nicolomecca <nico97.mecca@gmail.com>
2024-11-26 19:10:06 +0100

    modifica classi per lo stile

--- capstone-front-end/src/components/Animazione.jsx
+++ capstone-front-end/src/components/Animazione.jsx
@@ -1,52 +1,52 @@
 import { useEffect, useState } from 'react';
 
-const Animazione= ()=> {
-      const [isRotating, setIsRotating] = useState(true);
-    
-      useEffect(() => {
-        const timer = setTimeout(() => {
-          setIsRotating(false);
-        }, 3000);
-    
-        return () => clearTimeout(timer);
-      }, []);
-    
-      return (
-        <div className="bg-black min-vh-100">
-          <main className="container-fluid p-0">
-            <section className="row mx-0">
-              <div
-                className="col-12 d-flex justify-content-center align-items-center"
-                style={{ height: "90vh" }}
-              >
-                <div className={`cube-container ${isRotating ? 'rotating' : ''}`}>
-                  <div className="cube">
-                    <div className="face front">
-                      <span className="title-word text-white">Universal</span>
-                      <span className="title-word green-gradient-text">Code</span>
-                    </div>
-                    <div className="face back">
-                      <span className="purple-gradient-text">Learn How</span>
-                    </div>
-                    <div className="face right">
-                      <span className="green-gradient-text">Learn Code</span>
-                    </div>
-                    <div className="face left">
-                      <span className="title-word text-white">Develop</span>
-                    </div>
-                    <div className="face top">
-                      <span className="purple-gradient-text">Create</span>
-                    </div>
-                    <div className="face bottom">
-                      <span className="green-gradient-text">Build</span>
-                    </div>
+const Animazione = () => {
+    const [isRotating, setIsRotating] = useState(true);
+  
+    useEffect(() => {
+      const timer = setTimeout(() => {
+        setIsRotating(false);
+      }, 3000);
+  
+      return () => clearTimeout(timer);
+    }, []);
+  
+    return (
+      <div className="cube-bg-black min-vh-100">
+        <main className="container-fluid p-0">
+          <section className="row mx-0">
+            <div
+              className="col-12 d-flex justify-content-center align-items-center"
+              style={{ height: "90vh" }}
+            >
+              <div className={`cube-container ${isRotating ? 'cube-rotating' : ''}`}>
+                <div className="cube">
+                  <div className="cube-face cube-front">
+                    <span className="cube-text text-white">Universal</span>
+                    <span className="cube-text cube-green-text">Code</span>
+                  </div>
+                  <div className="cube-face cube-back">
+                    <span className="cube-purple-text">Learn How</span>
+                  </div>
+                  <div className="cube-face cube-right">
+                    <span className="cube-green-text">Learn Code</span>
+                  </div>
+                  <div className="cube-face cube-left">
+                    <span className="cube-text text-white">Develop</span>
+                  </div>
+                  <div className="cube-face cube-top">
+                    <span className="cube-purple-text">Create</span>
+                  </div>
+                  <div className="cube-face cube-bottom">
+                    <span className="cube-green-text">Build</span>
                   </div>
                 </div>
               </div>
-            </section>
-          </main>
-        </div>
-      );
-    };
+            </div>
+          </section>
+        </main>
+      </div>
+    );
+  };
     
 export default Animazione
--- capstone-front-end/src/components/Home.jsx
+++ capstone-front-end/src/components/Home.jsx
@@ -1,19 +1,18 @@
 const Home = () => {
   return (
-    <div className="bg-black min-vh-100">
+    <div className="home-bg-black min-vh-100">
       <main className="container-fluid p-0">
         <section className="row mx-0">
           <div
             className="col-12 d-flex justify-content-center align-items-center"
             style={{ height: "90vh" }}
           >
-            <div className="text-center title-animation">
-    
-              <h1 className="mega-title mb-4">
-                <span className="title-word text-white">Universal</span>
-                <span className="title-word green-gradient-text"> Code</span>
+            <div className="home-text-center home-title-animation">
+              <h1 className="home-mega-title mb-4">
+                <span className="home-title-word text-white">Universal</span>
+                <span className="home-title-word home-green-text"> Code</span>
               </h1>
-              <p className="purple-gradient-text fs-2 mt-5 subtitle-animation">
+              <p className="home-purple-text fs-2 mt-5 home-subtitle-animation">
                 Learn How. Learn Code.
               </p>
             </div>
@@ -23,4 +22,4 @@ const Home = () => {
     </div>
   );
 };
-export default Home;
+export default Home
